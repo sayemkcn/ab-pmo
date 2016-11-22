ad74fc9d1cc1b34560d6ca82388393a9c723dea7
Sayem Hossain <sayemkcn@gmail.com>
2016-11-22 23:54:17 +0600

    user register

---
+++ src/main/java/com/aimslabs/services/UserService.java
@@ -0,0 +1,18 @@
+package com.aimslabs.services;
+
+import com.aimslabs.domains.User;
+import com.aimslabs.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by sayemkcn on 11/22/16.
+ */
+@Service
+public interface UserService {
+
+    User getUser(Long id);
+    User saveUser(User user);
+
+
+}
---
+++ src/main/java/com/aimslabs/services/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.aimslabs.services;
+
+import com.aimslabs.domains.User;
+import com.aimslabs.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by sayemkcn on 11/22/16.
+ */
+@Component
+public class UserServiceImpl implements UserService {
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public User getUser(Long id) {
+        return userRepo.getOne(id);
+    }
+
+    @Override
+    public User saveUser(User user) {
+        return userRepo.saveAndFlush(user);
+    }
+
+}
