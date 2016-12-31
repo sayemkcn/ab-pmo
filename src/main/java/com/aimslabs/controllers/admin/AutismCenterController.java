6c40d2bf7622a5491cd7adfa3aba1a7a8d4d6671
Rezaul Alam <rezaul@agroho.com>
2016-12-31 11:07:27 +0600

    Committed For HTML Child

--- src/main/java/com/aimslabs/controllers/admin/AutismCenterController.java
+++ src/main/java/com/aimslabs/controllers/admin/AutismCenterController.java
@@ -1,7 +1,28 @@
 package com.aimslabs.controllers.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Author: Rezaul - Date: 29-Dec-16.
  */
+@Controller
+@RequestMapping("/admin/center")
 public class AutismCenterController {
+
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    public String centerHome() {
+        return "autism_center/center_home";
+    }
+
+    @RequestMapping(value = "/responses", method = RequestMethod.GET)
+    public String newResponsesFromOnlineScreening() {
+        return "autism_center/center_responses";
+    }
+
+    @RequestMapping(value = "/child", method = RequestMethod.GET)
+    public String showChildStatus() {
+        return "autism_center/center_child";
+    }
 }
